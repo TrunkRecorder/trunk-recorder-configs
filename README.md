540ee5c5230c61e0fee01eb4e445f40f5edd1ed2
Max Watermolen <mwateremolen@maxwelldps.com>
2022-04-02 11:23:40 -0500

    fixed folders

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Example config.json files for Trunk Recorder
 
 
 Name your config like so
-
 > SITE_NAME.SYSTEM_NAME.SITE_ID.Type[P25,Smartnet,dmr,conventional].json
 
 Example:
--- populate-folders.py
+++ populate-folders.py
@@ -1,4 +1,6 @@
 import requests, os
+from pathlib import Path
+
 
 # City|State short name|State full name|County|City Alias Mixed Case
 url = "https://raw.githubusercontent.com/grammakov/USA-cities-and-states/master/us_cities_states_counties.csv"
@@ -27,16 +29,22 @@ for location in locale_array:
 
         if not os.path.isdir(f"{cwd}/{state}/systems-misc"):
             os.mkdir(f"{cwd}/{state}/systems-misc")
+        Path(f'{cwd}/{state}/systems-misc/.gitkeep').touch()
 
         if not os.path.isdir(f"{cwd}/{state}/counties/{county}"):
             os.mkdir(f"{cwd}/{state}/counties/{county}")
+        Path(f'{cwd}/{state}/counties/{county}/.gitkeep').touch()
 
         # Create City Dirs
         if not os.path.isdir(f"{cwd}/{state}/counties/{county}/cities/"):
             os.mkdir(f"{cwd}/{state}/counties/{county}/cities/")
+        Path(f"{cwd}/{state}/counties/{county}/cities/.gitkeep").touch()
+
 
         if not os.path.isdir(f"{cwd}/{state}/counties/{county}/cities/{city}"):
             os.mkdir(f"{cwd}/{state}/counties/{county}/cities/{city}")
+        Path(f'{cwd}/{state}/counties/{county}/cities/{city}/.gitkeep').touch()
+
 
     except Exception as error:
         print(str(error))
